b6e78a836ca3b180b82f71e846a0ba4db58eb4c4
Akshaya Nallasivam <101433359+I-akshayaN@users.noreply.github.com>
2025-01-02 20:26:23 +0530

    Add files via upload

---
+++ fin_agent.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.model.openai import OpenAIChat
+from phi.tools.yfinance import YFinanceTools
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+
+# Initialize Agents
+web_agent = Agent(
+    name="myweb",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[DuckDuckGo()],
+    instructions=["Include all sources"],
+    show_tools_calls=True,
+    markdown=True,
+)
+
+finance_agent = Agent(
+    name="Finance Agent",
+    role="Get financial data",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True)],
+    instructions=["Use tables to display data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+multi_ai_agent = Agent(
+    team=[web_agent, finance_agent],
+    model=Groq(id="llama-3.1-70b-versatile"),
+    instructions=["Always include sources", "Use tables to display data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+multi_ai_agent.print_response("Summarize analyst recommendation  on TSLA")
+
